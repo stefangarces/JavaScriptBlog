b8b331b35978379b783efe330dbd5dca29e2a757
Stefan Garces <stefangarces@gmail.com>
2021-01-29 18:28:44 +0100

    Changed the design of the 'Add and remove' area, no counter, only 'add page / remove page'. It works. And also suceed to make the 'remove' button to remove one blogpost for each buttonpress.

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const knapp = document.querySelector(".add");
+const buttonAdd = document.querySelector("#add");
+const buttonRemove = document.querySelector("#remove");
 const post = document.querySelector(".container");
 
-knapp.addEventListener("click", function(){
+/*
+function addByOne(e) {
+    buttonRemove.innerHTML = ++i;
+}
 
-    const container = document.createElement("container")
+buttonRemove.addEventListener("click", function (e) {
+    addByOne(e);
+})
+*/
+
+buttonAdd.addEventListener("click", function() {
+
+    
+    const container = document.createElement("div")
     const outerDiv = document.createElement("div");
     const innerDiv = document.createElement("div");
     const title = document.createElement("h3");
-    const paragraph = document.createElement("p");
+    const paragraph = document.createElement("p")
+
+
 
     const titleText = document.createTextNode("Blog title here...");
     const paragraphText = document.createTextNode("Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus recusandae rem ipsum dolor quod, a repellendus aspernatur, ad vero incidunt dicta dolores eveniet officia esse laboriosam nisi magnam obcaecati quidem?")
@@ -27,23 +41,15 @@ knapp.addEventListener("click", function(){
     outerDiv.appendChild(innerDiv);
     container.appendChild(outerDiv);
 
+
     outerDiv.classList.add("flex-container");
 
     post.append(container);
+
 })
 
-// 'addClick' function, to add a number to the "counter" when the user click on the plus button to add another blogpost.
-// Had a bug in the beginning. I could only add up to number "1", but that was because the "var clickPlus = 0;"
-// was inside the function, and that made the function add the value "0" when the function was running.
-// const counter = document.getElementById("counter");
-    
-var clickPlus = 0;
-function addClick() {
-    clickPlus += 1;
-    document.getElementById("counter").innerHTML = clickPlus;
-}
+buttonRemove.addEventListener("click", function() {
 
-function removeClick() {
-    clickMinus = clickPlus -1;
-    document.getElementById("counter").innerHTML = clickMinus;
-}
+    let container = document.querySelector(".flex-container")
+    container.remove();
+})
